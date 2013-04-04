fbe42909230800af425f1d98497b2ae99432e8a7
JohnVehr <johnvehr@yahoo.com>
2013-04-04 09:26:42 -0400

    bunch o crap for woodbine site, someone punch me in my face please

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,22 +1,34 @@
 $(document).ready(function(){
   $('.two').hide();
   $('.three').hide();
+  $('.four').hide();
 
   $('#one').click(function(){
     $('.two').hide();
     $('.three').hide();
+    $('.four').hide();
     $('.one').fadeIn('slow');
   });
 
   $('#two').click(function(){
     $('.one').hide();
     $('.three').hide();
+    $('.four').hide();
     $('.two').fadeIn('slow');
   });
 
   $('#three').click(function() {
     $('.two').hide();
     $('.one').hide();
+    $('.four').hide();
     $('.three').fadeIn('slow');
   });
+
+  $('#four').click(function(){
+    $('.two').hide();
+    $('.three').hide();
+    $('.one').hide();
+    $('.four').fadeIn('slow');
+  });
+
 });
