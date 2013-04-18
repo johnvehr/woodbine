fff824d07b743597f06683f28e635796b9fc303b
JohnVehr <johnvehr@yahoo.com>
2013-04-17 21:12:15 -0400

    added logos on home page

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'bootstrap-sass', '2.1'
 gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
+gem 'activeadmin'
 gem 'bootstrap-will_paginate', '0.0.6'
 gem 'jquery-rails', '2.0.2'
 
@@ -26,7 +27,6 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
-  gem 'factory_girl_rails', '4.1.0'
   gem 'cucumber-rails', '1.2.1', :require => false
   gem 'database_cleaner', '0.7.0'
   # gem 'launchy', '2.1.0'
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,34 +1,8 @@
-$(document).ready(function(){
-  $('.two').hide();
-  $('.three').hide();
-  $('.four').hide();
-
-  $('#one').click(function(){
-    $('.two').hide();
-    $('.three').hide();
-    $('.four').hide();
-    $('.one').fadeIn('slow');
-  });
-
-  $('#two').click(function(){
-    $('.one').hide();
-    $('.three').hide();
-    $('.four').hide();
-    $('.two').fadeIn('slow');
-  });
-
-  $('#three').click(function() {
-    $('.two').hide();
-    $('.one').hide();
-    $('.four').hide();
-    $('.three').fadeIn('slow');
-  });
-
-  $('#four').click(function(){
-    $('.two').hide();
-    $('.three').hide();
-    $('.one').hide();
-    $('.four').fadeIn('slow');
-  });
-
+$(document).ready(function() {  
+ $('.step-one').hover(function(){
+  $('#step-one').slideToggle('slow');
+ }) 
+ $('.step-one').mouseleave(function(){
+  $('#step-one').hide();
+ })
 });
