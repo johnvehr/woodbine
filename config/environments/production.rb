8211c8fd756f9776ee746106b2230b6dc4654db2
JohnVehr <johnvehr@yahoo.com>
2013-06-20 13:30:03 -0400

    hm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Woodbine::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.active_support.deprecation = :silence
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
