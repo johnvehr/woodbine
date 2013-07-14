8fcb73d30715e25d4161cd4d512d4cf353aa1b92
JohnVehr <johnvehr@yahoo.com>
2013-07-14 17:25:56 -0400

    validate dispenser description

--- app/models/dispenser.rb
+++ app/models/dispenser.rb
@@ -1,6 +1,9 @@
 class Dispenser < ActiveRecord::Base
   attr_accessible :image, :case, :cat_type, :category, :stock, :weight, :description, :name
   mount_uploader :image, ImageUploader
+
+  validates :description, presence: true, length: { maximum: 400 }
+
    def option_type 
     option_type = {
       'Industrial Heavy Duty' => ['Water-Activated', 'Waterless'],
