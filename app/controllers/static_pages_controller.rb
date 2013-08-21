3637aaedc51b75605b60d57cd3958c0fe748e291
JohnVehr <johnvehr@yahoo.com>
2013-08-21 19:32:03 -0400

    lets just see

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,7 @@ class StaticPagesController < ApplicationController
   end
 
   def industrial_water
-    @products = Product.where(:cat_type => "Water-Activated").paginate(:page => params[:page], per_page: 5)
+    @products = Product.where(:cat_type => "Water-Activated").order("stocka asc").paginate(:page => params[:page], per_page: 5)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @products }
@@ -70,6 +70,7 @@ class StaticPagesController < ApplicationController
   end
 
   def soap_dispensers 
+    
     @dispensers = Dispenser.paginate(:page => params[:page], per_page: 5)
     respond_to do |format|
       format.html # index.html.erb
