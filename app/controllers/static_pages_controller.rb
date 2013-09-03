e65dd3e69d7d24432be2a646dae8a7e60e25c288
JohnVehr <johnvehr@yahoo.com>
2013-09-02 22:48:50 -0400

    fixed a.record

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   http_basic_authenticate_with :name => "ohio", :password => "foamfresh", :except => [:soon, :home, :about, :msds, :general, :antibacterial,:industrial, :industrial_water,:liquid_lotion_soap, :liquid_antibacterial_soap, :foam_soap, :foam_soap_antibacterial, :general_sanitizers, :sanitizer_hand_cleaner, :soap_dispensers, :private_label, :resources]
 
   def industrial
-    @products = Product.where(:cat_type => "Waterless")
+    @products = Product.where(:cat_type => "Waterless").order("stocka asc").paginate(:page => params[:page], per_page: 5)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @products }
